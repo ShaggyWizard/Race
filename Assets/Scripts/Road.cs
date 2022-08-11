848d42d978d8990387f3f4421ebe763e87b2cb4f
Viktor Hristoforov <viktor.hristoforov@mytona.com>
2022-08-11 19:16:05 +0700

    added road creator

--- Assets/Scripts/Road.cs
+++ Assets/Scripts/Road.cs
@@ -1,8 +1,74 @@
-using System.Collections;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using UnityEngine;
 
 public class Road
 {
+    public List<Vector2> Points { get; private set; }
+    public float _width { get; private set; }
 
+
+    public Road(Vector2[] points)
+    {
+        Points = new List<Vector2>();
+        Points.AddRange(points);
+    }
+
+    /// <summary>
+    /// Calculates edges based on given points and width
+    /// </summary>
+    /// <param name="points">Ordered points</param>
+    /// <param name="width">Desired width of road</param>
+    /// <returns></returns>
+    public static Vector2[] CalculateVertices(Vector2[] points, float width)
+    {
+        if (points.Length < 2) { return null; }
+
+        float halfWidth = width * 0.5f;
+        Vector2[] edges = new Vector2[points.Length * 2];
+
+        int pointIndex = 0;
+        int vertexIndex = 0;
+
+        //First two edges based only on next point
+        Vector2 direction = points[1] - points[0];
+        Vector2 left = new Vector2(-direction.y, direction.x).normalized * halfWidth;
+        edges[vertexIndex] = points[pointIndex] + left;
+        edges[vertexIndex + 1] = points[pointIndex] - left;
+        pointIndex++;
+        vertexIndex += 2;
+
+
+        //Calculate all not first and not last vertices
+        for (; pointIndex < points.Length - 1; pointIndex++, vertexIndex += 2)
+        {
+            Vector2 lastVector = points[pointIndex - 1] - points[pointIndex];
+            Vector2 nextVector = points[pointIndex + 1] - points[pointIndex];
+
+            Vector2 outDirection = (lastVector.normalized + nextVector.normalized).normalized;
+            if (outDirection.magnitude == 0)
+            {
+                outDirection = new Vector2(-nextVector.y, nextVector.x).normalized;
+            }
+
+            left = new Vector2(-nextVector.y, nextVector.x).normalized;
+            left = Vector2.Dot(left, outDirection) >= 0 ? outDirection : -outDirection;
+
+            float cos = Vector2.Dot(outDirection, lastVector.normalized);
+            float sin = Mathf.Sqrt(1 - (cos * cos));
+            float hypotenuse = halfWidth / sin;
+            left *= hypotenuse;
+
+            edges[vertexIndex] = points[pointIndex] + left;
+            edges[vertexIndex + 1] = points[pointIndex] - left;
+        }
+
+        //Last point based only on previous point
+        direction = points[points.Length - 1] - points[points.Length - 2];
+        left = new Vector2(-direction.y, direction.x).normalized * halfWidth;
+
+        edges[edges.Length - 2] = points[pointIndex] + left;
+        edges[edges.Length - 1] = points[pointIndex] - left;
+
+        return edges;
+    }
 }
--- Assets/Scripts/RoadCreator.cs
+++ Assets/Scripts/RoadCreator.cs
@@ -1,18 +1,83 @@
-using System.Collections;
+using System;
 using System.Collections.Generic;
 using UnityEngine;
 
+[RequireComponent(typeof(MeshFilter))]
+[RequireComponent(typeof(MeshRenderer))]
+[RequireComponent(typeof(MeshCollider))]
 public class RoadCreator : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField] private float _width;
+    [SerializeField] private Vector2[] _angleAndRange;
+
+
+    private Road _road;
+    private MeshFilter _meshFilter;
+    private MeshCollider _meshCollider;
+
+    private const float halfRotation = 180f;
+    private const float maxAngle = 179.9f;
+    void OnValidate()
     {
+        Vector2[] points = new Vector2[_angleAndRange.Length + 1];
         
+        for (int i = 0; i < _angleAndRange.Length; i++)
+        {
+            if (i != 0)
+            {
+                if (_angleAndRange[i].x < _angleAndRange[i - 1].x - halfRotation) { _angleAndRange[i].x = _angleAndRange[i - 1].x - maxAngle; }
+                if (_angleAndRange[i].x > _angleAndRange[i - 1].x + halfRotation) { _angleAndRange[i].x = _angleAndRange[i - 1].x + maxAngle; }
+            }
+            if (_angleAndRange[i].y < _width * 0.5f) { _angleAndRange[i].y = _width * 0.5f; }
+
+            Quaternion rotation = Quaternion.Euler(0, _angleAndRange[i].x, 0);
+
+            Vector3 vector = rotation * Vector3.forward * _angleAndRange[i].y;
+            points[i + 1] = points[i] + new Vector2(vector.x, vector.z);
+        }
+
+        _road = new Road(points);
+
+        if (_meshFilter == null) { _meshFilter = GetComponent<MeshFilter>(); }
+        if (_meshCollider == null) { _meshCollider = GetComponent<MeshCollider>(); }
+
+        Vector2[] vertices = Road.CalculateVertices(_road.Points.ToArray(), _width);
+        _meshFilter.mesh = CreateRoadMesh(vertices);
+        _meshCollider.sharedMesh = _meshFilter.sharedMesh;
     }
 
-    // Update is called once per frame
-    void Update()
+    private Mesh CreateRoadMesh(Vector2[] points)
     {
-        
+        if (points == null)
+        {
+            return null;
+        }
+        Vector3[] vertices = new Vector3[points.Length * 2];
+        int[] triangles = new int[2 * (points.Length - 1) * 3];
+        int t = 0;
+
+        for (int i = 0; i < points.Length - 3; i += 2, t += 6)
+        {
+            vertices[i] = new Vector3(points[i].x, 0, points[i].y);
+            vertices[i + 1] = new Vector3(points[i + 1].x, 0, points[i + 1].y);
+            vertices[i + 2] = new Vector3(points[i + 2].x, 0, points[i + 2].y);
+            vertices[i + 3] = new Vector3(points[i + 3].x, 0, points[i + 3].y);
+
+            triangles[t] = i;
+            triangles[t + 1] = i + 2;
+            triangles[t + 2] = i + 1;
+
+            triangles[t + 3] = i + 1;
+            triangles[t + 4] = i + 2;
+            triangles[t + 5] = i + 3;
+        }
+
+        Mesh mesh = new Mesh();
+        mesh.vertices = vertices;
+        mesh.triangles = triangles;
+
+        mesh.RecalculateNormals();
+
+        return mesh;
     }
 }
